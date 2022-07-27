252026ae0ced8cdf1d675c4c02991a0fdfbe588a
Vansh <vanshenrique.singh@gmail.com>
2022-07-27 11:59:04 +0530

    Multiplication added

--- calc1.c
+++ calc1.c
@@ -11,6 +11,10 @@ int subtract(int x,int y){
     return x-y;
 }
 
+int multiply(int x,int y){
+    return x*y;
+}
+
 int main(){
     #ifndef ONLINE_JUDGE
         freopen("input.txt", "r", stdin);
@@ -20,7 +24,8 @@ int main(){
     while(true){
         printf("1. Add\n");
         printf("2. Subtract\n");
-        printf("3. Exit\n")
+        printf("3. Multiply");
+        printf("4. Exit\n");
         printf("\nEnter your option\n");
         int x;
         scanf("%d",&x);
@@ -42,6 +47,14 @@ int main(){
             printf("Subtraction of %d & %d is: %d\n",a,b,subtract(a,b));
         }
         else if(x==3){
+            int a,b;
+            printf("Enter first number: \n");
+            scanf("%d",&a);
+            printf("Enter second number \n");
+            scanf("%d",&b);
+            printf("Subtraction of %d & %d is: %d\n",a,b,multiply(a,b));
+        }
+        else if(x==4){
             printf("\nExiting...");
             return 0;
         }
